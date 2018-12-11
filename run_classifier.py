0bd61c99e100dc610a712dabb00358911fb4b06f
b <brightmart@hotmail.com>
2018-12-11 21:15:54 +0800

    update

--- run_classifier.py
+++ run_classifier.py
@@ -414,7 +414,7 @@ class SentencePairClassificationProcessor(DataProcessor):
 
   def get_labels(self):
     """See base class."""
-    return ["0", "1"]
+    return ["-1","0", "1"]
 
   def _create_examples(self, lines, set_type):
     """Creates examples for the training and dev sets."""
