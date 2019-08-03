25dd29994cb79d7e1e0f8a8ebfcb5d5207ed7bf0
brightmart <brightmart@hotmail.com>
2019-08-03 22:50:20 +0800

    Create test_mll_unzip.py

---
+++ test_mll_unzip.py
@@ -0,0 +1,21 @@
+def zipextract(bucketname, zipfilename_with_path):
+
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucketname)
+
+    destination_blob_pathname = zipfilename_with_path
+
+    blob = bucket.blob(destination_blob_pathname)
+    zipbytes = io.BytesIO(blob.download_as_string())
+
+    if is_zipfile(zipbytes):
+        with ZipFile(zipbytes, 'r') as myzip:
+            for contentfilename in myzip.namelist():
+                contentfile = myzip.read(contentfilename)
+                blob = bucket.blob(zipfilename_with_path + "/" + contentfilename)
+                blob.upload_from_string(contentfile)
+ 
+source_zip_file='gs://bert_base_dir/pretrain_marriage_labor_loan_tfrecord/tf_mll_examples_all_tfrecord.zip'
+target_path='gs://bert_base_dir/pretrain_marriage_labor_loan_tfrecord/fowefkekelg23idsdfksdd_test_zip_dfwe'
+
+zipextract(target_path，source_zip_file)
